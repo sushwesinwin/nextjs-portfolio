959ae6d532a35220e2af7ec3a1714f4ae0f01181
Su Shwe Sin Win <sushwesinw@gmail.com>
2025-05-11 00:57:14 +0630

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/subdir',
+    trailingSlash: true, // helps with static exports
+};
 
 export default nextConfig;
